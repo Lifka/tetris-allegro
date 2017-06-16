335637a048d5d3276ea882b2c992d1c48d6aeea8
Lifka <javier@izquierdovera.com>
2017-06-16 14:16:06 +0200

    Init project with Clion

---
+++ source/Model/Piece.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lifka on 16/06/17.
+//
+
+#include "Piece.h"
---
+++ source/Model/Piece.h
@@ -0,0 +1,8 @@
+//
+// Created by lifka on 16/06/17.
+//
+
+#ifndef TETRIS_PIECE_H
+#define TETRIS_PIECE_H
+
+#endif //TETRIS_PIECE_H
